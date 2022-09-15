9e5324c82c9120462bf04779ead697f7f4a9af2f
artem.tsarev <artem.tsarev@stm-labs.ru>
2022-09-15 21:33:48 +0300

    Add another test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -10,4 +10,12 @@ describe("Pokedex", function () {
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
+
+  it("can go from main page to pokemon page", function () {
+    cy.visit("http://localhost:5000", {
+      timeout: 180000,
+    });
+    cy.contains("ivysaur").click();
+    cy.contains("chlorophyll");
+  });
 });
